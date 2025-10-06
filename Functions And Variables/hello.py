ab7a68a88b9ef197a8a1cc2c32fb34774f199140
Srikanth <padamatisrikanth@outlook.com>
2025-10-06 23:11:33 +0530

    updated the hello.py file

--- Functions And Variables/hello.py
+++ Functions And Variables/hello.py
@@ -1,1 +1,8 @@
-print('Hello World!')
+name = input("What is your name? ").strip().title()
+# print("hello, " + name)
+# print("hello, ", end = '')
+# print(name)
+# name = name.strip().title()
+print(f"hello, {name}")
+# Positional parameters
+# Named parameters
